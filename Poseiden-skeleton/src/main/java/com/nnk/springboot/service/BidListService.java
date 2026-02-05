892a82ae6325d96e0212c31439ad9b160bb06c45
Caillaud Mikael <caillaud.mikael.pro@gmail.com>
2026-02-05 14:30:12 +0100

    last

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/BidListService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/BidListService.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 
 import com.nnk.springboot.repositories.BidListRepository;
 import org.springframework.stereotype.Service;
+
 @Service
 public class BidListService {
 
